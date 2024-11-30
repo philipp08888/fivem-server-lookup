680812d5efa9f9ef3e520d20ce502108e270dac4
philipp08888 <admin@fledi.dev>
2024-11-30 17:10:14 +0100

    feat: update tests

--- tests/e2e/index.spec.ts
+++ tests/e2e/index.spec.ts
@@ -2,5 +2,8 @@ import test, { expect } from "@playwright/test";
 
 test("start page renders correctly", async ({ page }) => {
   await page.goto("/");
-  await expect(page).toHaveTitle(/How To - Explanation/);
+  await expect(page).toHaveTitle(/FiveM Server Lookup/);
+
+  const div = page.getByText("How To - Explanation");
+  await expect(div).toBeVisible({ timeout: 40000 });
 });
