69c11f8e9aed05e90ee4ac191acd069209804892
philipp08888 <admin@fledi.dev>
2024-12-09 17:42:27 +0100

    fix: footer typo

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <footer className="flex h-full flex-col items-center justify-center py-2">
       <p className="text-sm text-[#cccccc96]">
-        This website is not way affiliated with cfx or a FiveM server.
+        This website is not affiliated with cfx or a FiveM server.
       </p>
     </footer>
   );
