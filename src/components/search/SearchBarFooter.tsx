3e2ba7c8445e55567879dbd14f4137c401fae39a
philipp08888 <admin@fledi.dev>
2024-12-10 09:08:55 +0100

    feat: small searchbar improvements

--- src/components/search/SearchBarFooter.tsx
+++ src/components/search/SearchBarFooter.tsx
@@ -7,22 +7,22 @@ export const SearchBarFooter = (): React.JSX.Element => {
   return (
     <div className="flex flex-row justify-end gap-2 rounded-b-md border-t-2 border-[#555] bg-[#333] p-2">
       <div className="flex flex-row items-center gap-1">
-        <span className="select-none rounded-md bg-[#444] p-0.5 text-[#ccc] shadow-md">
+        <span className="select-none rounded-md bg-[#444] px-2 py-1 text-[#ccc] shadow-md">
           <ChevronUpIcon className="size-4" />
         </span>
-        <span className="select-none rounded-md bg-[#444] p-0.5 text-[#ccc] shadow-md">
+        <span className="select-none rounded-md bg-[#444] px-2 py-1 text-[#ccc] shadow-md">
           <ChevronDownIcon className="size-4" />
         </span>
         navigate
       </div>
       <div className="flex flex-row items-center gap-1">
-        <span className="select-none rounded-md bg-[#444] p-0.5 text-[#ccc] shadow-md">
+        <span className="select-none rounded-md bg-[#444] px-2 py-1 text-[#ccc] shadow-md">
           esc
         </span>
         Close
       </div>
       <div className="flex flex-row items-center gap-1">
-        <span className="select-none rounded-md bg-[#444] p-0.5 text-[#ccc] shadow-md">
+        <span className="select-none rounded-md bg-[#444] px-2 py-1 text-[#ccc] shadow-md">
           ↵
         </span>
         Return
